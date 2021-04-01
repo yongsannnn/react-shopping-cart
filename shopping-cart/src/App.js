0d9b24258a230c1bb18b2bdc6ddaf1f28dcc8762
yongsannnn <yong_s@live.com>
2021-04-01 03:14:41 +0000

    Added login component

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import { useState } from "react"
+// import axios from "axios"
+import LoginComponent from "./LoginComponent"
+
+
+export default function App() {
+    return (
+        <React.Fragment>
+            <LoginComponent/>
+        </React.Fragment>
+    );
+}
+
---
+++ shopping-cart/src/LoginComponent.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { useState } from "react"
+import axios from "axios"
+
+const baseUrl = "https://3000-aqua-canid-3knbtzpy.ws-us03.gitpod.io"
+
+export default function LoginComponent() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    return (
+        <React.Fragment>
+            <label>Email</label>
+            <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <label>Password</label>
+            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <button onClick={
+                async ()=> {
+                    const response = await axios.post(baseUrl + "/api/users/login", {
+                        "email": email,
+                        "password": password
+                    })
+                    // console.log(response.data)
+                    localStorage.setItem("accessToken", response.data.accessToken)
+                    localStorage.setItem("refreshToken", response.data.refreshToken)
+                    console.log(localStorage)
+                }
+            }>Log In</button>
+        </React.Fragment>
+    );
+}
+
